9d585e5d455b969456df1c5b94ae59fab166a0be
DiegoBHV <bryan71360@gmail.com>
2023-01-26 11:55:56 -0500

    grid

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const form = document.querySelector('#form');
 
-function alerta() {
-  alert ("Gracias por Completar el Formulario");
-}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let username = document.querySelector('#username').value
+  let email = document.querySelector('#email').value;
+  let tel = document.querySelector('#tel').value;
+  console.log(username, email, tel);
+
+});
+
+
+
 
 let id = (id) => document.getElementById(id);
 
@@ -9,7 +19,6 @@ let classes = (classes) => document.getElementsByClassName(classes);
 
 let username = id("username"),
   email = id("email"),
-  form = id("form"),
   errorMsg = classes("error"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
@@ -20,9 +29,9 @@ let username = id("username"),
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  engine(username, 0, "Username cannot be blank");
-  engine(email, 1, "Email cannot be blank");
-  engine(password, 2, "Password cannot be blank");
+  engine(username, 0, "Este campo es obligatorio")
+  engine(email, 1, "Este campo es obligatorio");
+  engine(tel, 2, "Este campo es obligatorio");
 });
 
 
@@ -42,19 +51,32 @@ let engine = (id, serial, message) => {
     errorMsg[serial].innerHTML = message;
     id.style.border = "2px solid orange";
 
-    // icons
-    failureIcon[serial].style.opacity = "1";
-    successIcon[serial].style.opacity = "0";
 
   } else {
     errorMsg[serial].innerHTML = "";
     id.style.border = "2px solid green";
+    alert("Gracias por Completar el Formulario");
 
-    // icons
-    failureIcon[serial].style.opacity = "0";
-    successIcon[serial].style.opacity = "1";
   }
 };
+
+const toggleButton = document.getElementById('button-menu')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click', function()  {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+});
+
+
+
 
 
 // enviar(CloseEvent){
